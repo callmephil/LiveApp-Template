5849c01e827059da23c57d48d4c3041cff2563eb
Callmephil <phil19-94@hotmail.fr>
2019-12-17 23:17:33 +0200

    Updates

---
+++ precache-manifest.2a541bacfeafccedcbe71195c970f568.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29b300e411864259282e2889b5adfd5b",
+    "url": "/LiveApp/index.html"
+  },
+  {
+    "revision": "2255695bf31096d98ff0",
+    "url": "/LiveApp/static/css/2.6771fd38.chunk.css"
+  },
+  {
+    "revision": "52b1407e3daad49f9751",
+    "url": "/LiveApp/static/css/main.a5141a15.chunk.css"
+  },
+  {
+    "revision": "2255695bf31096d98ff0",
+    "url": "/LiveApp/static/js/2.0572a61f.chunk.js"
+  },
+  {
+    "revision": "1961b8cc70e60764ec8bb8495ff13476",
+    "url": "/LiveApp/static/js/2.0572a61f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "52b1407e3daad49f9751",
+    "url": "/LiveApp/static/js/main.5919fc38.chunk.js"
+  },
+  {
+    "revision": "3c6395911e18a9458ec5",
+    "url": "/LiveApp/static/js/runtime-main.640b7b05.js"
+  }
+]);
